366df7360db01ec98cc9e30278a6ad6b2c99c22c
Carlos Rodriguez <carlos@eddorre.com>
2013-11-06 15:08:44 -0800

    Requiring veezi/configuration. Adding class methods configure and configuration.

--- lib/veezi.rb
+++ lib/veezi.rb
@@ -1,1 +1,20 @@
 require "veezi/version"
+require "veezi/configuration"
+
+module Veezi
+  class << self
+    def configure(config = nil)
+      if config
+        config.each do |k,v|
+          configuration.send("#{k}=", v) rescue nil if configuration.respond_to?("#{k}=")
+        end
+      end
+
+      yield(configuration) if block_given?
+    end
+
+    def configuration
+      @configuration ||= Veezi::Configuration.new
+    end
+  end
+end

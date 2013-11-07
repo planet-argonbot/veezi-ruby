edda7589b010007c0b565b067cf10c6476afb636
Carlos Rodriguez <carlos@eddorre.com>
2013-11-06 16:26:54 -0800

    Added REQUIRED_ATTRIBUTES as well as valid? method.

--- lib/veezi/configuration.rb
+++ lib/veezi/configuration.rb
@@ -1,10 +1,16 @@
 module Veezi
   class Configuration
+    REQUIRED_ATTRIUBTES = %w(api_key endpoint_url)
+
     attr_accessor :api_key
     attr_accessor :use_ssl
     attr_accessor :endpoint_url
-  end
 
-  def initialize
+    def initialize
+    end
+
+    def valid?
+      REQUIRED_ATTRIUBTES.all? { |attribute| !self.send(attribute).nil? }
+    end
   end
 end

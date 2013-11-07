de6220243856133339a8d6882e155bc8f5111dfc
Carlos Rodriguez <carlos@eddorre.com>
2013-11-06 22:20:39 -0800

    Added constructor method, configuration method, all method and protected request method to base api file. Constructor sets the client and the parser.

--- lib/veezi/api/base.rb
+++ lib/veezi/api/base.rb
@@ -1,6 +1,34 @@
+require 'rest_client'
+
 module Veezi
   module API
     class Base
+      attr_accessor :api_path
+      attr_reader :client
+      attr_reader :parser
+
+      def initialize(client)
+        @client = client
+        @parser = Veezi::API::Parser.new(self.client.configuration.content_type)
+      end
+
+      def configuration
+        self.client.configuration
+      end
+
+      def content_type
+        self.client.configuration.content_type || :json
+      end
+
+      def all
+        response = request(:get)
+        self.parser.parse(response)
+      end
+
+      protected
+      def request(method, options = {})
+        RestClient.send(method, self.configuration.endpoint_url + self.api_path, options.merge({ :accept => self.content_type }))
+      end
     end
   end
 end
